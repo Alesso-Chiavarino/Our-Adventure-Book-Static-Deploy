015346621e7dbd34d174e6540b992bc9288f5fe9
Alesso-Chiavarino <alessochiavarino@gmail.com>
2023-01-17 03:21:50 -0300

    letters fin2

--- src/api/Letter.api.js
+++ src/api/Letter.api.js
@@ -3,30 +3,30 @@ import axios from 'axios';
 export const getLettersRequest = (page) => {
 
     if(page) {
-        // return axios.get(`https://ouradventurebookdeploy-production.up.railway.app/api/letters?page=${page}`);
-        return axios.get(`http://localhost:4000/api/letters?page=${page}`);
+        return axios.get(`https://ouradventurebookdeploy-production.up.railway.app/api/letters?page=${page}`);
+        // return axios.get(`http://localhost:4000/api/letters?page=${page}`);
     }
     
-    // return axios.get('https://ouradventurebookdeploy-production.up.railway.app/api/letters');
-    return axios.get('http://localhost:4000/api/letters');
+    return axios.get('https://ouradventurebookdeploy-production.up.railway.app/api/letters');
+    // return axios.get('http://localhost:4000/api/letters');
 }
 
 export const getLetterRequest = (id) => {
-    // return axios.get(`https://ouradventurebookdeploy-production.up.railway.app/api/letters/${id}`);
-    return axios.get(`http://localhost:4000/api/letters/${id}`);
+    return axios.get(`https://ouradventurebookdeploy-production.up.railway.app/api/letters/${id}`);
+    // return axios.get(`http://localhost:4000/api/letters/${id}`);
 }
 
 export const createLetterRequest = (letter) => {
-    // return axios.post('https://ouradventurebookdeploy-production.up.railway.app/api/letters', letter);
-    return axios.post('http://localhost:4000/api/letters', letter);
+    return axios.post('https://ouradventurebookdeploy-production.up.railway.app/api/letters', letter);
+    // return axios.post('http://localhost:4000/api/letters', letter);
 }
 
 export const updateLetterRequest = (id, letter) => {
-    // return axios.put(`https://ouradventurebookdeploy-production.up.railway.app/api/letters/${id}`, letter);
-    return axios.put(`http://localhost:4000/api/letters/${id}`, letter);
+    return axios.put(`https://ouradventurebookdeploy-production.up.railway.app/api/letters/${id}`, letter);
+    // return axios.put(`http://localhost:4000/api/letters/${id}`, letter);
 }
 
 export const deleteLetterRequest = (id) => {
-    // return axios.delete(`https://ouradventurebookdeploy-production.up.railway.app/api/letters/${id}`);
-    return axios.delete(`http://localhost:4000/api/letters/${id}`);
+    return axios.delete(`https://ouradventurebookdeploy-production.up.railway.app/api/letters/${id}`);
+    // return axios.delete(`http://localhost:4000/api/letters/${id}`);
 }
