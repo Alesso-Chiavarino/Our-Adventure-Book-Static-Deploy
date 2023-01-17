d9796214b0eb981d8d976184ffb95193bf6aa343
Alesso-Chiavarino <alessochiavarino@gmail.com>
2023-01-17 15:44:35 -0300

    actu15 final

--- src/api/adventure.api.js
+++ src/api/adventure.api.js
@@ -1,17 +1,17 @@
 import axios from 'axios'
 
 export const getAdventuresRequest = async () => {
-    return await axios.get('https://our-adventure-book-api-deploy.vercel.app/api/adventure');
+    return await axios.get('https://our-adventure-book-api-rest.onrender.com/api/adventure');
     // return await axios.get('http://localhost:4000/api/adventure');
 }
 
 export const getLimitedAdventuresRequest = async (limit, page, search) => {
-    return await axios.get(`https://our-adventure-book-api-deploy.vercel.app/api/adventure?limit=${limit}&page=${page}&search=${search}`);
+    return await axios.get(`https://our-adventure-book-api-rest.onrender.com/api/adventure?limit=${limit}&page=${page}&search=${search}`);
     // return await axios.get(`http://localhost:4000/api/adventure?limit=${limit}&page=${page}&search=${search}`);
 }
 
 export const getAdventureRequest = async (id) => {
-    return await axios.get(`https://our-adventure-book-api-deploy.vercel.app/api/adventure/${id}`);
+    return await axios.get(`https://our-adventure-book-api-rest.onrender.com/api/adventure/${id}`);
     // return await axios.get(`http://localhost:4000/api/adventure/${id}`);
 }
 
@@ -24,7 +24,7 @@ export const createAdventureRequest = async (data) => {
     form.append('category', data.category)
     form.append('image', data.image)
 
-    return await axios.post('https://our-adventure-book-api-deploy.vercel.app/api/adventure', form, {
+    return await axios.post('https://our-adventure-book-api-rest.onrender.com/api/adventure', form, {
     // return await axios.post('http://localhost:4000/api/adventure', form, {
         headers: {
             'Content-Type': 'multipart/form-data'
@@ -40,7 +40,7 @@ export const updateAdventureRequest = async (id, data) => {
     form.append('category', data.category)
     form.append('image', data.image)
 
-    return await axios.put(`https://our-adventure-book-api-deploy.vercel.app/api/adventure/${id}`, form, {
+    return await axios.put(`https://our-adventure-book-api-rest.onrender.com/api/adventure/${id}`, form, {
     // return await axios.put(`http://localhost:4000/api/adventure/${id}`, form, {
         headers: {
             'Content-Type': 'multipart/form-data'
@@ -49,6 +49,6 @@ export const updateAdventureRequest = async (id, data) => {
 }
 
 export const deleteAdventureRequest = async (id) => {
-    return await axios.delete(`https://our-adventure-book-api-deploy.vercel.app/api/adventure/${id}`);
+    return await axios.delete(`https://our-adventure-book-api-rest.onrender.com/api/adventure/${id}`);
     // return await axios.delete(`http://localhost:4000/api/adventure/${id}`);
 }
