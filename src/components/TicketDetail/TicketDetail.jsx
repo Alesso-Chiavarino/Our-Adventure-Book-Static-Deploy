84d3d5a2a660792b8130eaa918a978a9e4386581
Alesso-Chiavarino <alessochiavarino@gmail.com>
2023-02-04 23:27:58 -0300

    ticket mobile

--- src/components/TicketDetail/TicketDetail.jsx
+++ src/components/TicketDetail/TicketDetail.jsx
@@ -59,23 +59,87 @@ const TicketDetail = () => {
     }
 
     const handleTouchMove = (e) => {
-        if (window.innerWidth <= 767) {
 
-            const { clientX } = e.touches[0];
-            const { clientY } = e.touches[0];
+        ticketRef.current.style.transform = transform;
 
-            const rotationX = ((clientY - halfHeight) / halfHeight) * -10;
-            const rotationY = ((clientX - halfWidth) / halfWidth) * 10;
-            const depth = ((clientY - halfHeight) / halfHeight) * 10;
+        const { clientX } = e.touches[0];
+        const { clientY } = e.touches[0];
 
-            setTransform(`rotateX(${rotationX}deg) rotateY(${rotationY}deg) translateZ(${depth}px) scale(1.1)`)
-        }
+        const rotationX = ((clientY - halfHeight) / halfHeight) * -10;
+        const rotationY = ((clientX - halfWidth) / halfWidth) * 10;
+        const depth = ((clientY - halfHeight) / halfHeight) * 10;
+
+        setTransform(`rotateX(${rotationX}deg) rotateY(${rotationY}deg) translateZ(${depth}px) scale(1.1)`)
     }
 
+    const [start, setStart] = useState({ x: 0, y: 0 });
+
+    const handleTouchStart = (e) => {
+        // e.preventDefault()
+        setStart({ x: e.touches[0].clientX, y: e.touches[0].clientY });
+    };
+
+    const handleTouchEnd = () => {
+        setTransform(`rotateX(0deg) rotateY(0deg) translateZ(0)`);
+        ticketRef.current.style.transform = `rotateX(0deg) rotateY(0deg) translateZ(0) `;
+    };
+
     const renderMain = () => {
-        
-        if (detectMobileDevice) {
+
+        if (detectMobileDevice()) {
             return (
+                //mobile
+                <div className="ticket-detail flex justify-center mx-5">
+                    <div
+                        className="wrapper"
+                        ref={wrapperRef}
+                        onTouchStart={(e) => handleTouchStart(e)}
+                        onTouchMove={(e) => handleTouchMove(e)}
+                        onTouchEnd={handleTouchEnd}
+                    >
+                        <div className="ticket-mobile" ref={ticketRef}>
+                            <article className="ticket-mobile bg-slate-50 flex flex-col">
+
+                                <div className="right px-2 py-5">
+                                    <h3 className='text-5xl font-semi-bold'>{ticket.title}</h3>
+                                    <span>solo valido para:</span>
+                                    <div className="flex gap-5">
+                                        <span className='text-2xl'>{ticket.date}</span>
+                                        <span className='text-2xl'>{ticket.hour}</span>
+                                    </div>
+                                    <h4 className='text-3xl'>Sala {ticket.room}</h4>
+                                    <div className="flex gap-1 items-end">
+                                        <span className='text-2xl'>Asiento:</span>
+                                        <h4 className='text-4xl'>{ticket.seat}</h4>
+
+                                    </div>
+                                    <div className="flex gap-5">
+                                        <span className='text-xl'>SHMAS2D</span>
+                                        <span className='text-xl font-semibold'>${ticket.price}</span>
+                                    </div>
+                                </div>
+
+                                <div className="left">
+                                    <div className='py-10'>
+                                        <h3 className='text-gray-400 font-semibold mb-5 text-3xl'>SHOWCASE</h3>
+                                        <div className="icon flex flex-col items-center justify-center">
+                                            <span className='text-gray-500 text-md font-bold'>SHOWCASE</span>
+                                            <span className='text-blue-300 font-bold text-3xl'>IMAX</span>
+                                            <span className='text-gray-500 text-md theatre font-bold'>THEATRE</span>
+                                        </div>
+                                    </div>
+                                </div>
+
+
+                            </article>
+                            <div className="bg"></div>
+                        </div>
+                    </div>
+                </div>
+            )
+        } else {
+            return (
+                //desktop
                 <div className="ticket-detail flex justify-center mx-5">
                     <div
                         className="wrapper"
@@ -84,10 +148,6 @@ const TicketDetail = () => {
                         onMouseLeave={() => {
                             setTransform(`rotateX(0deg) rotateY(0deg) translateZ(0)`)
                             ticketRef.current.style.transform = `rotateX(0deg) rotateY(0deg) translateZ(0) `;
-                        }}
-                        onTouchMove={(e) => handleTouchMove(e)}
-                        onTouchEnd={() => {
-                            setTransform(`rotateX(0deg) rotateY(0deg) translateZ(0)`)
                         }}
                     >
                         <div className="ticket" ref={ticketRef}>
@@ -130,8 +190,7 @@ const TicketDetail = () => {
                     </div>
                 </div>
             )
-        } else {
-            return <span>Mobile</span>
+
         }
     }
 
